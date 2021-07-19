92932876c3d60289bfc435eaf990c8f7cb3ec494
simonbucko <38184399+simonbucko@users.noreply.github.com>
2021-07-19 09:31:31 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,6 +14,8 @@ const reducers = combineReducers({
 });
 
 //, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware)/*, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/))
+const store = createStore(reducers, initialState, compose(applyMiddleware(...middleware), typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? a => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
 export default store;
